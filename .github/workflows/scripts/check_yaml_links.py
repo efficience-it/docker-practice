13aa58ab4cc6fc362ae2af430298194bad6c9f22
lacatoire <louis-arnaud.catoire@external.drivalia.com>
2025-07-28 11:25:00 +0200

    update script to check link in kubernetes

--- .github/workflows/scripts/check_yaml_links.py
+++ .github/workflows/scripts/check_yaml_links.py
@@ -25,7 +25,7 @@ def check_url(url):
 
     try:
         headers = {
-            "User-Agent": "Mozilla/5.0 (compatible; LinkChecker/1.0)"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
         }
         response = requests.head(url, headers=headers, allow_redirects=True, timeout=TIMEOUT)
         if response.status_code >= 400:
